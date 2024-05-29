465489862925ce1f80472e88bb84a6a3b85979d7
Christen <christen.foad@gmail.com>
2024-05-29 13:26:04 +0300

    Add project files.

---
+++ BlazorDay1Alex/Models/Department.cs
@@ -0,0 +1,9 @@
+﻿namespace BlazorDay1Alex.Models
+{
+    public class Department
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string MAnagerImage { get; set; }
+    }
+}
---
+++ BlazorDay1Alex/Pages/DepartmentComponent.razor.cs
@@ -0,0 +1,50 @@
+﻿using BlazorDay1Alex.Models;
+using System.Xml.Linq;
+
+namespace BlazorDay1Alex.Pages
+{
+    public partial class  DepartmentComponent
+    {
+        public int SelectDeptID { get; set; } = 3;
+
+        public List<Department> DEptList { get; set; }
+
+        public Department Department { get; set; }
+        
+        public DepartmentComponent()
+        {
+            DEptList = new List<Department>();
+            DEptList.Add(new Department()
+            {
+                Id = 1,
+                Name = "SD",
+                MAnagerImage = "2.jpg"
+            });
+            DEptList.Add(new Department()
+            {
+                Id = 2,
+                Name = "OS",
+                MAnagerImage = "m.png"
+            });
+            DEptList.Add(new Department()
+            {
+                Id = 3,
+                Name = "AI",
+                MAnagerImage = "2.jpg"
+            });
+
+
+            Department = new Department()
+            {
+                Id = 1,
+                Name = "SD",
+                MAnagerImage = "2.jpg"
+            };
+        }
+
+        private void LoadDEpt()
+        {
+            Department = DEptList.FirstOrDefault(d => d.Id == SelectDeptID);
+        }
+    }
+}
